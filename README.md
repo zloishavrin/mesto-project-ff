00eef89e79101b3e6e5c1f1a0d4e384634f25632
zloishvarin <nexxxusoffical@gmail.com>
2024-11-20 13:30:17 +0300

    first module: first iteration

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/zloishavrin/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция удаления карточки
+const deleteCard = (cardElement) => cardElement.remove();
 
-// @todo: Функция удаления карточки
+// Функция создания карточки
+const createCard = (card) => {
+  const { link: linkCard, name: nameCard } = card;
 
-// @todo: Вывести карточки на страницу
+  const element = cardTemplate.content
+    .querySelector('.places__item')
+    .cloneNode(true);
+  
+  const image = element.querySelector('.card__image');
+  const title = element.querySelector('.card__title');
+  const deleteButton = element.querySelector('.card__delete-button');
+
+  image.src = linkCard;
+  image.alt = nameCard;
+  title.textContent = nameCard;
+
+  deleteButton.addEventListener('click', () => deleteCard(element));
+
+  return element;
+}
+
+// Вывести карточки на страницу
+for(const card of initialCards) {
+  const cardElement = createCard(card);
+  placesList.append(cardElement);
+}
