cc2285f74c00515d575d0b3022da22e556fe92cf
zloishvarin <nexxxusoffical@gmail.com>
2024-11-21 09:56:56 +0300

    second module: second iteration - add webpack config

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,11 @@
+import '../pages/index.css';
+import initialCards from './cards';
+
 // Темплейт карточки
-const cardTemplate = document.querySelector('#card-template');
+const cardTemplate = document.querySelector("#card-template");
 
 // DOM узлы
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 // Функция удаления карточки
 const deleteCard = (cardElement) => cardElement.remove();
@@ -12,24 +15,24 @@ const createCard = (card) => {
   const { link: linkCard, name: nameCard } = card;
 
   const element = cardTemplate.content
-    .querySelector('.places__item')
+    .querySelector(".places__item")
     .cloneNode(true);
-  
-  const image = element.querySelector('.card__image');
-  const title = element.querySelector('.card__title');
-  const deleteButton = element.querySelector('.card__delete-button');
+
+  const image = element.querySelector(".card__image");
+  const title = element.querySelector(".card__title");
+  const deleteButton = element.querySelector(".card__delete-button");
 
   image.src = linkCard;
   image.alt = nameCard;
   title.textContent = nameCard;
 
-  deleteButton.addEventListener('click', () => deleteCard(element));
+  deleteButton.addEventListener("click", () => deleteCard(element));
 
   return element;
-}
+};
 
 // Вывести карточки на страницу
-for(const card of initialCards) {
+for (const card of initialCards) {
   const cardElement = createCard(card);
   placesList.append(cardElement);
 }
